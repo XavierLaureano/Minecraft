be26c686dba806dc619f6cd0d3068a83d14d1416
cristian <cristian@192.168.0.11>
2019-10-23 23:40:40 -0400

    Added Custom Block

---
+++ src/main/java/MinecraftMod/init/ModBlocks.java
@@ -0,0 +1,18 @@
+package MinecraftMod.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import MinecraftMod.Blocks.BlockBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+
+public class ModBlocks {
+	
+	public static List <Block> BLOCKS = new ArrayList<Block>();
+	
+	
+	public static final Block GALAXY_OPAL_BLOCK = new BlockBase("Galaxy Opal Block", Material.FIRE);
+
+}
--- src/main/java/MinecraftMod/util/handler/RegistryHandler.java
+++ src/main/java/MinecraftMod/util/handler/RegistryHandler.java
@@ -1,7 +1,10 @@
 package MinecraftMod.util.handler;
 
+import MinecraftMod.init.ModBlocks;
 import MinecraftMod.init.ModItems;
 import MinecraftMod.util.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -15,12 +18,23 @@ public class RegistryHandler {
 	public static void onItemRegister(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
 	}
+	
+	@SubscribeEvent
+	public static void onBlockRegister(RegistryEvent.Register<Block> event) {
+		event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
+	}
 
 	@SubscribeEvent
 	public static void onModelRegister(ModelRegistryEvent event) {
 		for (Item item : ModItems.ITEMS) {
 			if(item instanceof IHasModel) {
 				((IHasModel)item).registerModels();
+			}
+		}
+		
+		for (Block block : ModBlocks.BLOCKS) {
+			if(block instanceof IHasModel) {
+				((IHasModel)block).registerModels();
 			}
 		}
 
